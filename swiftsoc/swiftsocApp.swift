0ecd690283a613f32d863a3cc7507a7eb137dc0e
unaivan <unaivan@gmail.com>
2023-07-27 08:22:12 +0700

    Initial Commit

---
+++ swiftsoc/swiftsocApp.swift
@@ -0,0 +1,17 @@
+//
+//  swiftsocApp.swift
+//  swiftsoc
+//
+//  Created by una ivan on 27/07/23.
+//
+
+import SwiftUI
+
+@main
+struct swiftsocApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
